7952eab35ea7203c7ba9e3b2ee311d4b7a319b6e
jeremy <jeremy.zavala@usm.cl>
2025-06-17 02:57:54 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Carreras from './pages/Carreras';
@@ -11,27 +12,62 @@ import BecasAdmision from './pages/BecasAdmision';
 import ProgramasApoyo from './pages/ProgramasApoyo';
 import AdmisionEspecial from './pages/AdmisionEspecial';
 import AdmisionDirecta from './pages/AdmisionDirecta';
+import logo from './assets/images/logo.png';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/carreras" element={<Carreras />} />
-          <Route path="/carreras/:carreraId" element={<CarreraDetalle />} />
-          <Route path="/aranceles" element={<Aranceles />} />
-          <Route path="/admision-paes" element={<AdmisionPAES />} />
-          <Route path="/desarrollo" element={<UnderDevelopment />} />
-          <Route path="/beneficios-ministeriales" element={<BeneficiosMinisteriales />} />
-          <Route path="/becas-admision" element={<BecasAdmision />} />
-          <Route path="/programas-apoyo" element={<ProgramasApoyo />} />
-          <Route path="/admision-especial" element={<AdmisionEspecial />} />
-          <Route path="/admision-directa" element={<AdmisionDirecta />} />
-        </Routes>
-      </div>
+      <Box>
+        <Box sx={{ 
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: '60px',
+          bgcolor: '#003366',
+          display: 'flex',
+          alignItems: 'center',
+          px: 2,
+          zIndex: 1000
+        }}>
+          <Box sx={{ 
+            width: 200, // Ancho fijo para el contenedor del logo
+            height: '100%',
+            bgcolor: '#003366', // Mismo color de fondo que el header
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            <img 
+              src={logo} 
+              alt="Logo USM" 
+              style={{ 
+                height: '40px',
+                marginRight: '16px'
+              }} 
+            />
+          </Box>
+          <Navbar />
+        </Box>
+        <Box sx={{ mt: '60px' }}> {/* Añadimos margen superior para compensar la barra fija */}
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/carreras" element={<Carreras />} />
+              <Route path="/carreras/:carreraId" element={<CarreraDetalle />} />
+              <Route path="/aranceles" element={<Aranceles />} />
+              <Route path="/admision-paes" element={<AdmisionPAES />} />
+              <Route path="/desarrollo" element={<UnderDevelopment />} />
+              <Route path="/beneficios-ministeriales" element={<BeneficiosMinisteriales />} />
+              <Route path="/becas-admision" element={<BecasAdmision />} />
+              <Route path="/programas-apoyo" element={<ProgramasApoyo />} />
+              <Route path="/admision-especial" element={<AdmisionEspecial />} />
+              <Route path="/admision-directa" element={<AdmisionDirecta />} />
+            </Routes>
+          </div>
+        </Box>
+      </Box>
     </Router>
   );
 }
